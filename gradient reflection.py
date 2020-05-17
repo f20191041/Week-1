3669d98e5487494a1d22229fe9a745140ea4119f
lehitsai05 <f20190672g@goa.bits-pilani.ac.in>
2020-05-17 21:35:22 +0530

    final

--- gradient reflection.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt 
-grad=np.zeros((1000,1000,3),dtype=np.uint8)
-val=np.linspace(0,255,500)
-for x in range(500):
-    grad[x:1000-x,x:1000-x]=val[x]
-plt.imshow(grad)
-plt.show()
-#print(grad)
